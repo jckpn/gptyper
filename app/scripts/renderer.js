b402f7fe54ccc0645bdc19f826b7862695a78d1e
Jack Paine <jacp3@outlook.com>
2024-09-29 17:29:22 +0100

    initial commit

--- app/scripts/renderer.js
+++
@@ -1,8 +0,0 @@
- $(function() {
-    // just focus editor in case it's not already
-    window.quillEditor.focus();
-
-    setTimeout(function() {
-        $('.loading-overlay').removeClass('first-loading-overlay-active');
-    }, 400);
-});
--- app/scripts/startup-checks.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    let apiKey = window.localStorage.getItem('apiKey') || '';
-    console.log('apiKey:', apiKey);
-    if (apiKey == '') {
-        // click 'set api key' button
-        $('.gpt-menu-option-apikey').click();
-    }
-});
